512eee03b665b470a3f36ee0e2a1629c2c369844
santoshkumarsubedi <subedisantos9@gmail.com>
2018-07-25 20:55:38 +0545

    finally

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http=require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/',function(req,res){
-  res.send(__dirname+'/index.html');
+  res.sendFile(__dirname+'/index.html');
 });
 
 http.listen(process.env.PORT||8080,function(){
