906e20bae2ac2e3092d89b7964340ff5f67fa695
santoshkumarsubedi <subedisantos9@gmail.com>
2018-07-25 20:42:29 +0545

    send message instead of file

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ var http=require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html');
-});
-io.on('connection',function(socket){
-  console.log('a user connected');
+  res.send(__dirname+'/index.html');
 });
 
 http.listen(8080,function(){
