5f44cb3693d7f07e9d7622aa69f57076c969a8e6
santoshkumarsubedi <subedisantos9@gmail.com>
2018-07-25 23:58:08 +0545

    done some improvement

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 var app=require('express')();
 var http=require('http').Server(app);
 var io = require('socket.io')(http);
-
+var uuid=require('node-uuid');
+var clients=[];
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
 });
 
+
+io.on('connection',function(socket){
+  console.log("client connected");
+var client_uuid=uuid.v4();
+var nickname=client_uuid.substr(0,8);
+clients.push({"id":client_uuid,"ws":socket,"nickname":nickname});
+io.emit('chat message',nickname+" is connected")
+socket.on('chat message',function(msg){
+  for(i=0;i<clients.length;i++){
+    var client_socket=clients[i].ws;
+    if(client_socket==socket){
+      client_socket.emit('chat message',"me>"+msg);
+    }else{
+    client_socket.emit('chat message',nickname+">"+msg);
+  }
+}
+});
+
+socket.on('disconnect',function(ev){
+    for(var i=0;i<clients.length;i++){
+      if(clients[i].id==client_uuid){
+        console.log('client [%s] disconnected',nickname);
+        clients.splice(i,1);
+      }
+    }
+    io.emit("chat message",nickname+"is disconnected");
+  });
+
+});
 http.listen(process.env.PORT||8080,function(){
   console.log('listening on *:3000');
 });
