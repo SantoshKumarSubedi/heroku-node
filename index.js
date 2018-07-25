a0d0347e6c2395185d878c8e8d51f69c90f54c45
santoshkumarsubedi <subedisantos9@gmail.com>
2018-07-26 00:01:09 +0545

    done some improvements

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app=require('express')();
 var http=require('http').Server(app);
 var io = require('socket.io')(http);
-var uuid=require('node-uuid');
+var uuid=require('uuid');
 var clients=[];
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
